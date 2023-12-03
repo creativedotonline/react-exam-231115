69a9d34114204622e37376b9e46b39b9f88d23c7
creativedotonline <creativedotonline@gmail.com>
2023-12-03 13:51:33 +0200

    testimonials styling

--- client/src/components/testimonials/SingleTestimonial.jsx
+++ client/src/components/testimonials/SingleTestimonial.jsx
@@ -1,6 +1,7 @@
 import "../post/PostStyle.css"
 import { Link } from 'react-router-dom';
 import "./TestimonialsStyle.css"
+import { formatDate } from "../../utils/dataUtils";
 
 
 const SingleTestimonial  = ({
@@ -8,14 +9,18 @@ const SingleTestimonial  = ({
 	img,
 	description,
 	_id,
+	_createdOn,
 }) => {
 	return(
 		<>
 		<div className="single-post-list" id={_id} >
+			<div className="single-post-list-feature">
 			<div className="single-post-list-image">
 				<img className="single-post-list-img-top" src={img} />
 			</div>
+			<span>{formatDate(_createdOn)}</span></div>
 			<div className="single-post-list-body">
+
 				<div className="single-post-list-title">
 					<h3>{title}</h3>
 				</div>
